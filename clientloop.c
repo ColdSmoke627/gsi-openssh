b8323bfcc1867e10c150b2ec5912bc6c883dfd6d
Venkat Yekkirala <ysvenkat@ncsa.illinois.edu>
2016-11-17 11:34:48 -0600

    Fix for the X11Forwarding issue on Mac OS.

--- clientloop.c
+++ clientloop.c
@@ -319,6 +319,10 @@ client_x11_get_proto(const char *display, const char *xauth_path,
 	struct stat st;
 	u_int now, x11_timeout_real;
 
+#if __APPLE__
+        int is_path_to_socket = 0;
+#endif /* __APPLE__ */
+
 	xauthdir = xauthfile = NULL;
 	*_proto = proto;
 	*_data = data;
@@ -334,6 +338,35 @@ client_x11_get_proto(const char *display, const char *xauth_path,
 			debug("x11_get_proto: DISPLAY not set");
 			return;
 		}
+#if __APPLE__
+               {
+                       /*
+                        * If using launchd socket, remove the screen number from the end
+                        * of $DISPLAY. is_path_to_socket is used later in this function
+                        * to determine if an error should be displayed.
+                        */
+                       char path[PATH_MAX];
+                       struct stat sbuf;
+
+                       strlcpy(path, display, sizeof(path));
+                       if (0 == stat(path, &sbuf)) {
+                               is_path_to_socket = 1;
+                       } else {
+                               char *dot = strrchr(path, '.');
+                               if (dot) {
+                                       *dot = '\0';
+                                       /* screen = atoi(dot + 1); */
+                                       if (0 == stat(path, &sbuf)) {
+                                               is_path_to_socket = 1;
+                                               debug("x11_get_proto: $DISPLAY is launchd, removing screennum");
+                                               setenv("DISPLAY", path, 1);
+                                       }
+                               }
+                       }
+               }
+#endif /* __APPLE__ */
+
+
 		/*
 		 * Handle FamilyLocal case where $DISPLAY does
 		 * not match an authorization entry.  For this we
@@ -425,6 +458,9 @@ client_x11_get_proto(const char *display, const char *xauth_path,
 	if (!got_data) {
 		u_int32_t rnd = 0;
 
+#if __APPLE__
+		if (!is_path_to_socket)
+#endif /* __APPLE__ */
 		logit("Warning: No xauth data; "
 		    "using fake authentication data for X11 forwarding.");
 		strlcpy(proto, SSH_X11_PROTO, sizeof proto);
